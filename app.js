97485a819ada5a3e233c47df301b1b1b2f75a150
markygan123 <macawilemarkygan@gmail.com>
2020-07-10 22:17:34 -0500

    add dice images, replace numbers with image

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const side = document.querySelector(".side");
 const btn = document.querySelector("button");
+let dice = document.createElement("img");
 
 
 function roll() {
-    side.innerHTML = Math.floor(Math.random() * 6) + 1;
+    let diceRoll = Math.floor(Math.random() * 6) + 1;
+    if (diceRoll === 2) {
+        dice.src = "images/2.gif";
+        side.appendChild(dice);
+    } else if (diceRoll === 3) {
+        dice.src = "images/3.gif";
+        side.appendChild(dice);
+    } else if (diceRoll === 4) {
+        dice.src = "images/4.gif";
+        side.appendChild(dice);
+    } else if (diceRoll === 5) {
+        dice.src = "images/5.gif";
+        side.appendChild(dice);
+    } else if (diceRoll === 6) {
+        dice.src = "images/6.gif";
+        side.appendChild(dice);
+    } else {
+        dice.src = "images/1.gif";
+        side.appendChild(dice);
+    }
 }
 
 btn.addEventListener("click", function () {
-    let rollTime = setInterval(roll, 80);
+    let rollTime = setInterval(roll, 100);
     setTimeout(() => {
         clearTimeout(rollTime);
     }, 2000);
