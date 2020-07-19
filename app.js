ab24a52048e5599f0b3b92f539e81f2c0d0f4d01
markygan123 <macawilemarkygan@gmail.com>
2020-07-19 10:42:41 -0500

    add showDice function and removed if else statement from getRandomNumber

--- app.js
+++ app.js
@@ -3,32 +3,38 @@ const btn = document.querySelector("button");
 let dice = document.createElement("img");
 
 
-function roll() {
-    let diceRoll = Math.floor(Math.random() * 6) + 1;
-    if (diceRoll === 2) {
-        dice.src = "images/2.gif";
-        side.appendChild(dice);
-    } else if (diceRoll === 3) {
-        dice.src = "images/3.gif";
-        side.appendChild(dice);
-    } else if (diceRoll === 4) {
-        dice.src = "images/4.gif";
-        side.appendChild(dice);
-    } else if (diceRoll === 5) {
-        dice.src = "images/5.gif";
-        side.appendChild(dice);
-    } else if (diceRoll === 6) {
-        dice.src = "images/6.gif";
-        side.appendChild(dice);
-    } else {
-        dice.src = "images/1.gif";
-        side.appendChild(dice);
-    }
+function getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+    // if (diceRoll === 2) {
+    //     dice.src = "images/2.gif";
+    //     side.appendChild(dice);
+    // } else if (diceRoll === 3) {
+    //     dice.src = "images/3.gif";
+    //     side.appendChild(dice);
+    // } else if (diceRoll === 4) {
+    //     dice.src = "images/4.gif";
+    //     side.appendChild(dice);
+    // } else if (diceRoll === 5) {
+    //     dice.src = "images/5.gif";
+    //     side.appendChild(dice);
+    // } else if (diceRoll === 6) {
+    //     dice.src = "images/6.gif";
+    //     side.appendChild(dice);
+    // } else {
+    //     dice.src = "images/1.gif";
+    //     side.appendChild(dice);
+    // }
+}
+
+function showDice() {
+    let randomNumber = getRandomNumber();
+    dice.src = `images/${randomNumber}.gif`;
+    side.appendChild(dice);
 }
 
 btn.addEventListener("click", function () {
     side.classList.remove("result", "result-one");
-    let rollTime = setInterval(roll, 100);
+    let rollTime = setInterval(showDice, 100);
     setTimeout(() => {
         clearTimeout(rollTime);
         if (dice.src.match("1.gif")) {
